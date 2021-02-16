0b0cb95f79ae5d6afee930f15968dba9d12e96a0
Curtis Bridges <curtis@curtisbridges.com>
2021-02-16 09:37:32 -0500

    fix webpack config
    Signed-off-by: Curtis Bridges <curtis@curtisbridges.com>

---
+++ prod.config.js
@@ -0,0 +1,30 @@
+const path = require('path')
+
+module.exports = (env) => {
+  return {
+    mode: 'production',
+    entry: './src/app.js',
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js',
+    },
+    module: {
+      rules: [
+        {
+          loader: 'babel-loader',
+          test: /\.js$/,
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.s?css$/,
+          use: ['style-loader', 'css-loader', 'sass-loader'],
+        },
+      ],
+    },
+    devtool: 'source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true,
+    },
+  }
+}
