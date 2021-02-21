507f5e8304217d71d60f31aa96fcf44311c9f6ea
Curtis Bridges <curtis@curtisbridges.com>
2021-02-21 10:41:18 -0500

    add startRemoveExpense and tests
    Signed-off-by: Curtis Bridges <curtis@curtisbridges.com>

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -4,6 +4,7 @@ import {
   startAddExpense,
   addExpense,
   editExpense,
+  startRemoveExpense,
   removeExpense,
   setExpenses,
   startSetExpenses,
@@ -131,4 +132,27 @@ test('should fetch the expense from firebase', (done) => {
     })
     done()
   })
+})
+
+test('should remove expense from firebase', (done) => {
+  const store = createMockStore({})
+
+  const id = expenses[2].id
+
+  store
+    .dispatch(startRemoveExpense({ id }))
+    .then(() => {
+      const actions = store.getActions()
+
+      expect(actions[0]).toEqual({
+        type: 'REMOVE_EXPENSE',
+        id,
+      })
+
+      return database.ref(`expenses/${id}`).once('value')
+    })
+    .then((snapshot) => {
+      expect(snapshot.val()).toBeFalsy()
+      done()
+    })
 })
